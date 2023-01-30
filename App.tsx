eb383f8a10e1ea05e7c14d8695de48fe1ff42629
Lauri <lauri.iivarinen@gmail.com>
2023-01-30 15:09:07 +0200

    app.tsx

--- App.tsx
+++ App.tsx
@@ -1,29 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from'@react-navigation/native';
-import { createBottomTabNavigator } from'@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Homepage from './components/Homepage';
 import Settings from './components/Settings';
 
+const Tab = createBottomTabNavigator();
+
 export default function App() {
-
-  const Tab = createBottomTabNavigator()
-
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Homepage" component={Homepage}/> {/*REPLACE*/}
-        <Tab.Screen name="Settings" component={Settings}/> {/*REPLACE*/}
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
+  <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home"component={Homepage} />
+      <Tab.Screen name="Settings"component={Settings} />
+    </Tab.Navigator>
+    </NavigationContainer>  
+);}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
