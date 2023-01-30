94f350357097e5eb8427eb379d1e4f5e3d6ff86a
Lauri <lauri.iivarinen@gmail.com>
2023-01-30 14:56:41 +0200

    merge 1 yey

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from'@react-navigation/native';
+import { createBottomTabNavigator } from'@react-navigation/bottom-tabs';
 
 export default function App() {
+
+  const Tab = createBottomTabNavigator()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Homepage" component={App}/> {/*REPLACE*/}
+        <Tab.Screen name="Settings" component={App}/> {/*REPLACE*/}
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
