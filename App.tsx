ab55e4146d34a02b61b09420ba0a3b38ce1d3a51
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2023-02-02 12:27:22 +0200

    stack nav

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from'@react-navigation/native-stack';
+import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
 import HomepageTabNav from './components/HomepageTabNav';
 import QueuingScreen from './components/QueuingScreen';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Homepage" component={HomepageTabNav}></Stack.Screen>
-        <Stack.Screen name="Queue" component={QueuingScreen}></Stack.Screen>
+        <Stack.Screen name="QueuingScreen" component={QueuingScreen}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
 
--- components/HomepageTabNav.tsx
+++ components/HomepageTabNav.tsx
@@ -7,12 +7,10 @@ const Tab = createBottomTabNavigator();
 
 const HomepageTabNav = () => {
     return(
-    <NavigationContainer independent={true}>
         <Tab.Navigator>
             <Tab.Screen name="Home" component={Homepage} />
             <Tab.Screen name="Settings" component={Settings} />
         </Tab.Navigator>
-    </NavigationContainer>
 )}
 
 export default HomepageTabNav
