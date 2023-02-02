3d5833055113a4e245ba6cb41ea76a40b77c5383
Juho9 <raketti96@gmail.com>
2023-02-02 11:20:01 +0200

    Queuing screen

---
+++ components/QueuingScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const QueuingScreen = () => {
+  return (
+    <View>
+        <Text>
+            Waiting for players...
+        </Text>
+    </View>
+  )
+}
+
+export default QueuingScreen
