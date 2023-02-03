0d3912272e3334e3b190dba1271efda836cbd795
samz9n <samuel.langhoff@outlook.com>
2023-02-03 12:10:29 +0200

    added bg-image and small changes to settings page

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, ImageBackground, StyleSheet } from 'react-native'
 
 const Settings = () => {
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <View style={styles.container}>
+    <ImageBackground source={require('./images/settingsBgImage.png')} resizeMode="cover" style={styles.image}>
+      <Text style={styles.text}>Theme</Text>
+    </ImageBackground>
+  </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
+});
 
 export default Settings
