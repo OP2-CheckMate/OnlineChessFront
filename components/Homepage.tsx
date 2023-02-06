db995e56cea746c940475ac8e982eeacff147e68
Jouni Johansson <bgu211@myy.haaga-helia.fi>
2023-02-06 15:54:45 +0200

    setting button

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -1,68 +1,71 @@
 
-import { View, Text, Button, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
 
-const Homepage = ({ navigation } : any) => {
+const Homepage = ({ navigation }: any) => {
 
 
   return (
     <>
       <View style={styles.container}>
-        <ImageBackground source={require('./images/ChessGameFrontpagePic.png')} resizeMode="cover" style={styles.image}>
-        <Text style={styles.title}>Welcome to Chess Game!</Text>
+        <ImageBackground source={require('./images/ChessGameFrontpagePic2.jpg')} resizeMode="cover" style={styles.image}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('Settings')}
+              style={styles.appButtonContainer}
+            >
+              <Text style={styles.appButtonText}>Settings</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('QueuingScreen')}
+              style={styles.appButtonContainer}
+            >
+              <Text style={styles.appButtonText}>Start Game</Text>
+            </TouchableOpacity>
+          </View>
         </ImageBackground>
       </View>
-      <View style={styles.buttonContainer}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('QueuingScreen')}
-        style={styles.appButtonContainer}
-      >
-      <Text style={styles.appButtonText}>Start Game</Text>
-      </TouchableOpacity>
-      </View>
+
     </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 4,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonContainer: {
+    flexDirection: 'column',
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'flex-end',
     justifyContent: 'center',
-    backgroundColor: 'rgb(1, 27, 10)'
-    
-    
   },
+
   appButtonContainer: {
     elevation: 8,
     backgroundColor: "rgb(1, 27, 10)",
-    borderRadius: 50,
+    borderRadius: 10,
     borderColor: "white",
     paddingVertical: 10,
     paddingHorizontal: 12,
-    
+    width: '45%'
+
   },
   appButtonText: {
-    fontSize: 30,
-    color: "#fff",
+    fontSize: 20,
+    color: "white",
     fontWeight: "bold",
     alignSelf: "center",
     textTransform: "uppercase"
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-around',
+    marginBottom: 50
   },
+
   image: {
     flex: 1,
     width: '100%'
-    
+
   }
 });
 
