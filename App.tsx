bea45d4a8f50d028ffa3dc57c7cdb5a3ed163b82
samz9n <samuel.langhoff@outlook.com>
2023-02-08 13:28:32 +0200

    fixed input and made custom buttom for queue page

--- App.tsx
+++ App.tsx
@@ -3,21 +3,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Homepage from './components/Homepage';
 import QueuingScreen from './components/QueuingScreen';
 import Settings from './components/Settings';
-import Game from './components/Game';
+/* import Game from './components/Game'; */
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Homepage" component={Homepage} />
-        <Stack.Screen name="QueuingScreen" component={QueuingScreen} />
-        <Stack.Screen name="Settings" component={Settings} />
-        <Stack.Screen name="Game" component={Game} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Homepage" component={Homepage} />
+				<Stack.Screen name="QueuingScreen" component={QueuingScreen} />
+				<Stack.Screen name="Settings" component={Settings} />
+				{/* <Stack.Screen name="Game" component={Game} /> */}
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 };
 
 export default App;
--- components/QueuingScreen.tsx
+++ components/QueuingScreen.tsx
@@ -1,19 +1,56 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, ImageBackground, Button, TextInput } from 'react-native';
+import { View, StyleSheet, ImageBackground, Button, TextInput, Pressable, Text } from 'react-native';
 
 const QueuingScreen = () => {
 	const [ name, setName ] = useState('');
+	const [ isDisabled, setIsDisabled ] = useState(true);
 
-	const searchOpponent = () => {
+	const findGame = () => {
+		//Fetch opponent & start game
 		console.log('Search opponent');
 	};
 
 	return (
 		<View style={styles.container}>
 			<ImageBackground source={require('./images/settingsBgImage.png')} resizeMode="cover" style={styles.image}>
-				<View>
-					<TextInput style={styles.input} placeholder="Enter your name" />
-					<Button disabled={name === ''} title="Search opponent" onPress={searchOpponent} />
+				<View style={styles.innerView}>
+					<TextInput
+						style={styles.input}
+						onChangeText={(text) => {
+							setName(text);
+							if (text !== '') {
+								setIsDisabled(false);
+							} else {
+								setIsDisabled(true);
+							}
+						}}
+						value={name}
+						placeholder="Enter your name"
+						autoCapitalize="words"
+						autoComplete="off"
+						autoFocus={true}
+						placeholderTextColor="rgb(110,93,53)"
+					/>
+					{/* custom made button with pressable component, so the button looks exactly the same in android and iOS */}
+					<View style={styles.btnOuterContainer}>
+						<Pressable
+							//if the button is disabled, the color will be grey
+							//when pressed, the button will be a little bit lighter, otherwise go with the default style
+							style={({ pressed }) => {
+								if (isDisabled) {
+									return [ styles.disabledBtn ];
+								} else {
+									return pressed
+										? [ styles.btnInnerContainer, styles.pressed ]
+										: styles.btnInnerContainer;
+								}
+							}}
+							onPress={findGame}
+							disabled={isDisabled}
+						>
+							<Text style={isDisabled ? styles.disabledbtnText : styles.btnText}>Find Game</Text>
+						</Pressable>
+					</View>
 				</View>
 			</ImageBackground>
 		</View>
@@ -30,11 +67,54 @@ const styles = StyleSheet.create({
 	},
 	input: {
 		height: 40,
+		width: 200,
 		margin: 12,
-		borderWidth: 1
+		borderWidth: 1,
+		borderRadius: 8,
+		borderColor: 'rgb(3,15,6)',
+		elevation: 5,
+
+		backgroundColor: 'rgba(255, 255, 255, 0.8)',
+		textAlign: 'center',
+		fontSize: 20
 	},
 	innerView: {
-		flex: 1
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	btnOuterContainer: {
+		borderRadius: 8,
+		margin: 4,
+		overflow: 'hidden',
+		elevation: 4,
+		shadowColor: 'black',
+		shadowOffset: { width: 0, height: 10 },
+		shadowOpacity: 0.25,
+		shadowRadius: 4
+	},
+	btnInnerContainer: {
+		backgroundColor: '#72063c',
+		paddingVertical: 12,
+		paddingHorizontal: 14
+	},
+	btnText: {
+		color: 'white',
+		fontWeight: 'bold',
+		fontSize: 16
+	},
+	pressed: {
+		opacity: 0.75
+	},
+	disabledBtn: {
+		backgroundColor: '#9e9e9e',
+		paddingVertical: 12,
+		paddingHorizontal: 14
+	},
+	disabledbtnText: {
+		color: 'rgb(124, 123, 123)',
+		fontWeight: 'bold',
+		fontSize: 16
 	}
 });
 
