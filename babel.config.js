4551889857a1797c5a687f1220d31fc439cdbb2e
AaltonenSan <santtu.36@gmail.com>
2023-02-09 14:32:30 +0200

    reanimated plugin changed to last item on plugins

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,13 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin',["module:react-native-dotenv", {
-      "envName": "APP_ENV",
-      "moduleName": "@env",
-      "path": ".env",
-    }]]
+    plugins: [
+      "module:react-native-dotenv", {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env",
+      },
+      'react-native-reanimated/plugin'
+    ]
   };
 };
