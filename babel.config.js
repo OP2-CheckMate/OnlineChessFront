dae9b228ab01af0018ffbc8716948e8720f0a0ce
GunnarSven <94739044+GunnarSven@users.noreply.github.com>
2023-02-09 16:05:09 +0200

    Modified babel.config

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,11 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      "module:react-native-dotenv", {
+      ["module:react-native-dotenv", {
         "envName": "APP_ENV",
         "moduleName": "@env",
         "path": ".env",
-      },
+      }],
       'react-native-reanimated/plugin' //HAS TO BE LAST IN PLUGINS LIST
     ]
   };
