b27c1f7eb676fad010a9186637d3635deec4c79d
Lauri <lauri.iivarinen@gmail.com>
2023-02-09 14:17:58 +0200

    babel.config and packege-lock

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
-module.exports = {
+module.exports = function (api) {
+  api.cache(true);
+  return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ["module:react-native-dotenv", {
-        "envName": "APP_ENV",
-        "moduleName": "@env",
-        "path": ".env",
-      }]
-    ],
+    plugins: [['react-native-reanimated/plugin'],["module:react-native-dotenv", {
+      "envName": "APP_ENV",
+      "moduleName": "@env",
+      "path": ".env",
+    }]]
+  };
 };
