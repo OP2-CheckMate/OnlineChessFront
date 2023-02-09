d811f244275dea12282e9d587d70cc25033c1cf2
AaltonenSan <santtu.36@gmail.com>
2023-02-09 13:07:23 +0200

    App.tsx changes

--- App.tsx
+++ App.tsx
@@ -3,21 +3,25 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Homepage from './components/Homepage';
 import QueuingScreen from './components/QueuingScreen';
 import Settings from './components/Settings';
-/* import Game from './components/Game'; */
+import Game from './components/Game';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="Homepage" component={Homepage} />
-				<Stack.Screen name="QueuingScreen" component={QueuingScreen} />
-				<Stack.Screen name="Settings" component={Settings} />
-				{/* <Stack.Screen name="Game" component={Game} /> */}
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Homepage" component={Homepage} />
+          <Stack.Screen name="QueuingScreen" component={QueuingScreen} />
+          <Stack.Screen name="Settings" component={Settings} />
+          <Stack.Screen name="Game" component={Game} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
+  );
 };
 
 export default App;
