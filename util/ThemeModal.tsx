40359229168c993d0ab99f1f3550a4034844a2eb
samz9n <samuel.langhoff@outlook.com>
2023-03-01 11:35:15 +0200

    working on theme selection, added radiobuttons

--- util/ThemeModal.tsx
+++ util/ThemeModal.tsx
@@ -1,13 +1,41 @@
 import { View, Text, Modal, Pressable, Alert, StyleSheet, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
-import RadioForm from 'react-native-simple-radio-button';
+import RadioForm, { RadioButton, RadioButtonInput, RadioButtonLabel } from 'react-native-simple-radio-button';
 
-interface Props {
+interface ModalProps {
 	isVisible: boolean;
 	closeModal: () => void;
 }
+interface SquareProps {
+	color1: string;
+	color2: string;
+}
 
-const ThemeModal = ({ isVisible, closeModal }: Props) => {
+const Themes = [
+	{ label: 'Default', value: 0, col1: 'rgb(176, 157, 144)', col2: 'rgb(74, 67, 62)' },
+	{ label: 'Fin', value: 1, col1: 'white', col2: 'darkblue' },
+	{ label: 'Swe', value: 2, col1: 'yellow', col2: 'darkblue' }
+];
+
+const Square = ({ color1, color2 }: SquareProps) => {
+	return (
+		<View style={styles.colorBox}>
+			<View style={{ height: 20, width: 20, backgroundColor: color1 }} />
+			<View style={{ height: 20, width: 20, backgroundColor: color2 }} />
+			<View style={{ height: 20, width: 20, backgroundColor: color2 }} />
+			<View style={{ height: 20, width: 20, backgroundColor: color1 }} />
+		</View>
+	);
+};
+
+const ThemeModal = ({ isVisible, closeModal }: ModalProps) => {
+	const [ selectedTheme, setSelectedTheme ] = useState(0);
+
+	const onThemeChange = (value: number) => {
+		setSelectedTheme(value);
+		console.log('pressed btn with value: ' + value);
+	};
+
 	return (
 		<View style={styles.centeredView}>
 			<Modal animationType="slide" transparent={true} visible={isVisible} onRequestClose={closeModal}>
@@ -15,18 +43,39 @@ const ThemeModal = ({ isVisible, closeModal }: Props) => {
 					<View style={styles.centeredView}>
 						<View style={styles.modalView}>
 							<Text style={styles.modalText}>Select Theme</Text>
-							{/* radiobuttons for theme-selection */}
-							<RadioForm
-								radio_props={[ { label: 'Light', value: 0 }, { label: 'Dark', value: 1 } ]}
-								initial={0}
-								onPress={(value) => {
-									console.log(value);
-								}}
-							/>
-							{/* close-button for modal if needed */}
-							{/* <Pressable style={[ styles.button, styles.buttonClose ]} onPress={closeModal}>
-								<Text style={styles.textStyle}>Close</Text>
-							</Pressable> */}
+							<RadioForm>
+								{Themes.map((theme) => (
+									<View style={styles.themeWrapper} key={theme.value}>
+										<RadioButton>
+											<RadioButtonInput
+												obj={theme}
+												index={theme.value}
+												isSelected={selectedTheme === theme.value}
+												onPress={() => {
+													onThemeChange(theme.value);
+												}}
+												buttonInnerColor={'rgb(30, 92, 46)'}
+												buttonOuterColor={
+													selectedTheme === theme.value ? 'rgb(30, 92, 46)' : '#000'
+												}
+												buttonSize={30}
+												buttonOuterSize={40}
+												buttonStyle={{}}
+											/>
+											<RadioButtonLabel
+												obj={theme}
+												index={theme.value}
+												onPress={() => {
+													onThemeChange(theme.value);
+												}}
+												labelStyle={{ fontSize: 20, color: '#02421d' }}
+												labelWrapStyle={{}}
+											/>
+											<Square color1={theme.col1} color2={theme.col2} />
+										</RadioButton>
+									</View>
+								))}
+							</RadioForm>
 						</View>
 					</View>
 				</TouchableOpacity>
@@ -34,6 +83,15 @@ const ThemeModal = ({ isVisible, closeModal }: Props) => {
 		</View>
 	);
 };
+
+{
+	/* close-button for modal if needed */
+}
+{
+	/* <Pressable style={[ styles.button, styles.buttonClose ]} onPress={closeModal}>
+<Text style={styles.textStyle}>Close</Text>
+</Pressable> */
+}
 
 const styles = StyleSheet.create({
 	centeredView: {
@@ -73,6 +131,18 @@ const styles = StyleSheet.create({
 	modalText: {
 		marginBottom: 15,
 		textAlign: 'center'
+	},
+	colorBox: {
+		height: 40,
+		width: 40,
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		border: 1,
+		borderColor: 'black',
+		borderWidth: 0.5
+	},
+	themeWrapper: {
+		/* justifyContent: 'center' */
 	}
 });
 
