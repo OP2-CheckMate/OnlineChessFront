37b37a134c4e697a7b4c55c3fe04876d6503e4f2
Lauri <lauri.iivarinen@gmail.com>
2023-03-06 14:46:23 +0200

    lobby screen shows player names

--- components/LobbyCodeScreen.tsx
+++ components/LobbyCodeScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, ImageBackground} from "react-native";
+import { View, Text, ImageBackground, StyleSheet} from "react-native";
 import {Lobby} from '../types/types'
 import CustomButton from "../util/CustomButton";
 
@@ -7,12 +7,19 @@ import CustomButton from "../util/CustomButton";
 //another player to join the lobby
 const LobbyCodeScreen = ({ route, navigation }: any) => {
     const { lobby, playerName } = route.params
+    console.log(lobby)
     return(
         <View style={{flex: 1}}>
             <ImageBackground source={require('./images/settingsBgImage.png')} resizeMode="cover" style={{flex: 1}}>
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'rgba(255,255,255,0.7)', margin: 50, borderRadius: 10}}>
+            <View style={styles.background}>
+                <View style={styles.center}>
                 <Text style={{fontSize: 45}}>LOBBY CODE:</Text>
                 <Text style={{fontSize: 65}}>{lobby.lobbyId}</Text>
+                </View>
+                <View style={styles.center}>
+                    <Text style={{fontSize: 20}}>{lobby.player1.name} VS. {lobby.player2? lobby.player2.name : 'TBD'}</Text>
+                </View>
+                
             </View>
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
                 <CustomButton title="Open Board" onPress={() => navigation.navigate('Game', {lobby, playerName})}></CustomButton>
@@ -21,5 +28,21 @@ const LobbyCodeScreen = ({ route, navigation }: any) => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:'rgba(255,255,255,0.7)',
+        margin: 50,
+        borderRadius: 10
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
 
 export default LobbyCodeScreen
