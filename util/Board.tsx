e68656e688460fe0519b12b6bbb09163f2aa9191
samz9n <samuel.langhoff@outlook.com>
2023-03-09 10:40:29 +0200

    refactored Board.tsx file

--- util/Board.tsx
+++ util/Board.tsx
@@ -1,7 +1,8 @@
 import { Text, View, StyleSheet, Dimensions } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Themes } from './BoardThemes';
+import { useIsFocused } from '@react-navigation/native';
 
 interface RowProps {
 	row: number;
@@ -10,17 +11,15 @@ interface SquareProps extends RowProps {
 	col: number;
 }
 
-const Row = ({ row }: RowProps) => {
-	return (
-		<View style={{ flex: 1, flexDirection: 'row' }}>
-			{new Array(8).fill(0).map((_, col) => <Square row={row} col={col} key={col} />)}
-		</View>
-	);
-};
-
-const Square = ({ row, col }: SquareProps) => {
+export default function Board() {
 	const [ colorOne, setColorOne ] = useState('');
 	const [ colorTwo, setColorTwo ] = useState('');
+	const isFocused = useIsFocused();
+
+	useEffect(() => {
+		getTheme();
+	}, []);
+
 	const getTheme = async () => {
 		try {
 			let value = await AsyncStorage.getItem('theme');
@@ -32,24 +31,32 @@ const Square = ({ row, col }: SquareProps) => {
 			console.log(error);
 		}
 	};
-	/* we call getTheme() in the Square component, so that the colors of the 
-  board are updated whenever the player changes the theme */
-	getTheme();
-	const offset = row % 2 === 0 ? 1 : 0;
-	const backgroundColor = (col + offset) % 2 === 0 ? colorTwo : colorOne;
-	const color = (col + offset) % 2 === 0 ? colorOne : colorTwo;
 
-	return (
-		<View style={{ flex: 1, backgroundColor: backgroundColor, padding: 5 }}>
-			<Text style={{ color: color, opacity: col === 0 ? 1 : 0 }}>{8 - row}</Text>
-			<Text style={{ color: color, opacity: row === 7 ? 1 : 0, alignSelf: 'flex-end' }}>
-				{String.fromCharCode('a'.charCodeAt(0) + col)}
-			</Text>
-		</View>
-	);
-};
+	const Row = ({ row }: RowProps) => {
+		return (
+			<View style={{ flex: 1, flexDirection: 'row' }}>
+				{new Array(8).fill(0).map((_, col) => <Square row={row} col={col} key={col} />)}
+			</View>
+		);
+	};
 
-export default function Board() {
+	const Square = ({ row, col }: SquareProps) => {
+		/* we call getTheme() in the useEffect, so that the colors of the 
+	  board are updated whenever the player changes the theme */
+
+		const offset = row % 2 === 0 ? 1 : 0;
+		const backgroundColor = (col + offset) % 2 === 0 ? colorTwo : colorOne;
+		const color = (col + offset) % 2 === 0 ? colorOne : colorTwo;
+
+		return (
+			<View style={{ flex: 1, backgroundColor: backgroundColor, padding: 5 }}>
+				<Text style={{ color: color, opacity: col === 0 ? 1 : 0 }}>{8 - row}</Text>
+				<Text style={{ color: color, opacity: row === 7 ? 1 : 0, alignSelf: 'flex-end' }}>
+					{String.fromCharCode('a'.charCodeAt(0) + col)}
+				</Text>
+			</View>
+		);
+	};
 	return <View style={styles.container}>{new Array(8).fill(0).map((_, row) => <Row row={row} key={row} />)}</View>;
 }
 const { width } = Dimensions.get('window');
