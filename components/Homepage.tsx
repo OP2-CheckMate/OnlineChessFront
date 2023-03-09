13b9cb4d8ab06bc28d6c500a45c89ea4ef51bdc0
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2023-03-09 11:59:07 +0200

    Homepage buttons re organized and changed background to rgba

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -3,24 +3,23 @@ import { View, Text, StyleSheet, ImageBackground, TouchableOpacity } from 'react
 
 const Homepage = ({ navigation }: any) => {
 
-
   return (
     <>
       <View style={styles.container}>
         <ImageBackground source={require('./images/ChessGameFrontpagePic2.jpg')} resizeMode="cover" style={styles.image}>
-          <View style={{flex: 2}}></View>
+          <View style={{flex: 5}}></View>
           <View style={styles.buttonContainer}>
+          <TouchableOpacity
+              onPress={() => navigation.navigate('QueuingScreen')}
+              style={styles.appButtonContainer}
+            >
+              <Text style={styles.appButtonText}>Start Game</Text>
+            </TouchableOpacity>
             <TouchableOpacity
               onPress={() => navigation.navigate('Settings')}
               style={styles.appButtonContainer}
             >
               <Text style={styles.appButtonText}>Settings</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => navigation.navigate('QueuingScreen')}
-              style={styles.appButtonContainer}
-            >
-              <Text style={styles.appButtonText}>Start Game</Text>
             </TouchableOpacity>
             <TouchableOpacity
               onPress={() => navigation.navigate('Game', {lobby: {lobbyId: 0, player1: 'p1'}, playerName: 'p1'})}
@@ -29,7 +28,7 @@ const Homepage = ({ navigation }: any) => {
               <Text style={styles.appButtonText}>Board</Text>
             </TouchableOpacity>
           </View>
-          <View style={{flex: 1}}></View>
+          
         </ImageBackground>
       </View>
 
@@ -47,10 +46,10 @@ const styles = StyleSheet.create({
 
   appButtonContainer: {
     elevation: 8,
-    backgroundColor: "#8A7C4A",
+    backgroundColor: "rgba(138,124,74, 0.85)",
     borderRadius: 10,
     borderColor: "rgb(1, 27, 10)",
-    borderWidth: 2,
+    borderWidth: 3,
     paddingVertical: 10,
     paddingHorizontal: 12,
     width: '55%'
@@ -64,7 +63,7 @@ const styles = StyleSheet.create({
     textTransform: "uppercase"
   },
   buttonContainer: {
-    flex: 1,
+    flex: 2,
     alignItems: 'center',
     justifyContent:'space-evenly',
     marginBottom: 50
