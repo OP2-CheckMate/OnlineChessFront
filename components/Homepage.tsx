15a3e1d4b715c22d464e1a70c16342044ac0d4d5
samz9n <samuel.langhoff@outlook.com>
2023-03-13 15:21:52 +0200

    tweaked frontpage button layout and styling

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -7,23 +7,22 @@ const Homepage = ({ navigation }: any) => {
     <>
       <View style={styles.container}>
         <ImageBackground source={require('./images/ChessGameFrontpagePic2.jpg')} resizeMode="cover" style={styles.image}>
-          <View style={{flex: 5}}></View>
           <View style={styles.buttonContainer}>
           <TouchableOpacity
               onPress={() => navigation.navigate('QueuingScreen')}
-              style={styles.appButtonContainer}
+              style={styles.appButton}
             >
               <Text style={styles.appButtonText}>Start Game</Text>
             </TouchableOpacity>
             <TouchableOpacity
               onPress={() => navigation.navigate('Settings')}
-              style={styles.appButtonContainer}
+              style={styles.appButton}
             >
               <Text style={styles.appButtonText}>Settings</Text>
             </TouchableOpacity>
             <TouchableOpacity
               onPress={() => navigation.navigate('Game', {lobby: {lobbyId: 0, player1: 'p1'}, playerName: 'p1'})}
-              style={styles.appButtonContainer}
+              style={styles.appButton}
             >
               <Text style={styles.appButtonText}>Board</Text>
             </TouchableOpacity>
@@ -44,29 +43,34 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  appButtonContainer: {
-    elevation: 8,
-    backgroundColor: "rgba(138,124,74, 0.85)",
+  appButton: {
+    backgroundColor: "rgba(138,124,74, 0.88)",
     borderRadius: 10,
-    borderColor: "rgb(1, 27, 10)",
-    borderWidth: 3,
     paddingVertical: 10,
     paddingHorizontal: 12,
-    width: '55%'
+    width: '55%',
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: 'rgba(170, 170, 170, 0.5)',
+    shadowColor: 'white',
+    shadowOpacity: 1,
+    shadowOffset: {width: 2, height: 2},
+    shadowRadius: 10,
+    elevation: 40,
 
   },
   appButtonText: {
-    fontSize: 20,
+    fontSize: 24,
     color: "white",
     fontWeight: "bold",
     alignSelf: "center",
-    textTransform: "uppercase"
+    textTransform: "uppercase",
   },
   buttonContainer: {
-    flex: 2,
+    flex: 1,
     alignItems: 'center',
-    justifyContent:'space-evenly',
-    marginBottom: 50
+    justifyContent:'center',
+    marginTop: 200,
   },
 
   image: {
