19400d6ea64c1cc008e8dba26edcf61eec167b1d
Jouni Johansson <bgu211@myy.haaga-helia.fi>
2023-04-24 16:21:13 +0300

    reversing last push as it did not work properly + openboard button now disabled if both player2 is not found yet

--- components/LobbyCodeScreen.tsx
+++ components/LobbyCodeScreen.tsx
@@ -20,11 +20,13 @@ const LobbyCodeScreen: FC<Props> = ({ route, navigation }) => {
   const [player2Name, setPlayer2Name] = useState<string>('TBD')
   const [snackIsVisible, setSnackIsVisible] = useState(false)
   const [lobby, setLobby] = useState(route.params.lobby)
+  const [openBoardEnabled, setOpenBoardEnabled] = useState(!!lobby.player2);
 
   socket.emit('joinroom', lobby.lobbyId)
   socket.on('playerJoined', (result: Lobby) => {
     setPlayer2Name(result.player2!.name)
     setLobby(result)
+    setOpenBoardEnabled(true);
     setSnackIsVisible(true)
     setTimeout(() => {
       setSnackIsVisible(false)
@@ -61,6 +63,7 @@ const LobbyCodeScreen: FC<Props> = ({ route, navigation }) => {
           <CustomButton
             title='Open Board'
             onPress={openBoard} 
+            disabled={!openBoardEnabled}
          />      
         </View>
       </ImageBackground>
--- socket/socket.tsx
+++ socket/socket.tsx
@@ -1,7 +1,8 @@
 import {io} from 'socket.io-client'
-import { HOST_NAME } from '@env'
+import { HOST_NAME} from '@env';
 //Reset cache for HOST_NAME -> npx expo start --clear
 
-const socket = io('http://' + HOST_NAME + ':8080')
+const localSocket = `http://${HOST_NAME}:8080`;
+const socket = io(localSocket);
 
-export default socket
+export default socket;
