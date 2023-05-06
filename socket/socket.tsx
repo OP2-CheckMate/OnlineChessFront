4b3154c0deda1bafa06f5d4d594d42d82c9922e9
Lauri <94760484+Lauri-Iivarinen@users.noreply.github.com>
2023-05-06 14:23:27 +0300

    Update socket.tsx

--- socket/socket.tsx
+++ socket/socket.tsx
@@ -2,8 +2,8 @@ import {io} from 'socket.io-client'
 import { HOST_NAME } from '@env'
 //Reset cache for HOST_NAME -> npx expo start --clear
 
-//const local = 'http://87.92.66.115:8080'
+//const local = ''
 
-const socket = io('http://87.94.154.110:8080')
+const socket = io(HOST NAME)
 
 export default socket
